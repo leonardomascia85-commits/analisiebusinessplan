98cbf98d6b31ee5b2262b10007a1bd7993fe2efa
leonardomascia85-commits <leonardomascia85@gmail.com>
2026-06-11 07:53:16 +0200

    Update genera-pdf.js

--- api/genera-pdf.js
+++ api/genera-pdf.js
@@ -921,14 +921,12 @@ module.exports = async function handler(req, res) {
   let browser = null;
   try {
     const html = buildReportHTML(data, config || {});
-  browser = await puppeteer.launch({
-      args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox'],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        'https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar'
-      ),
-      headless: chromium.headless,
-    });
+browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath(),
+  headless: 'new',
+});
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
     const pdf = await page.pdf({ format: 'A4', printBackground: true, margin: { top: 0, right: 0, bottom: 0, left: 0 } });
